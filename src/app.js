5b48f7c5fceb404dfe3f5a7b45924fda9865cd1a
BatiOli9 <oliversafdieh@gmail.com>
2022-12-22 23:30:51 -0300

    Seguro Sprint Finalizado

--- src/app.js
+++ src/app.js
@@ -12,3 +12,38 @@ app.get('/', function(req, res) {
     let htmlPath = path.resolve(__dirname, './views/index.html')
     res.sendFile(htmlPath)
 });
+
+app.get('/login.html', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/login.html')
+    res.sendFile(htmlPath)
+});
+
+app.get('/register.html', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/register.html')
+    res.sendFile(htmlPath)
+});
+
+app.get('/carrito.html', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/carrito.html')
+    res.sendFile(htmlPath)
+});
+
+app.get('/producto1.html', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/producto1.html')
+    res.sendFile(htmlPath)
+});
+
+app.get('/producto2.html', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/producto2.html')
+    res.sendFile(htmlPath)
+});
+
+app.get('/producto3.html', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/producto3.html')
+    res.sendFile(htmlPath)
+});
+
+app.get('/producto4.html', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/producto4.html')
+    res.sendFile(htmlPath)
+});
